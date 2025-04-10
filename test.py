965ec0ebc5f8a783aa91f8e427e56622bf673029
utyhm <utyhm@student.kit.edu>
2025-04-10 14:12:18 +0200

    Subplot 4

---
+++ test.py
@@ -0,0 +1,21 @@
+print("This is file 1")
+print(50)
+ 
+y = 3
+x = y+1
+print(x)
+
+werteliste = [ 30,4, 12, 56, 29]
+gesamt = 0
+for zahlenwert in werteliste: 
+    gesamt = gesamt + zahlenwert
+
+print(gesamt)
+
+orte = ["Kopenhagen", "Kapstadt", "Sydney"]
+stadt = "Sydney"
+if stadt in orte: 
+    print("Die Stadt", stadt, "ist in der Liste der Orte enthalten")
+
+
+
